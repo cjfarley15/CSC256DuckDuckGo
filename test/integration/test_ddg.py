c732f9c1ff0a55106ca1232d6f18dd9264737f45
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 19:26:49 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -12,7 +12,7 @@ def test_true():
 
 
 def test_ddg0():
-    resp = requests.get(url_ddg + "/?q="Presidents of The United States"&format=json")
+    resp = requests.get(url_ddg + "/?q=Presidents of the United States&format=json")
     rsp_data = resp.json()
     assert "Presidents of the United States" in rsp_data["RelatedTopics"]
 
