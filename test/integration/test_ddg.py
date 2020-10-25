77ba9833d49aedcd3d26addaa10a979e4e429a31
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 19:19:54 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -14,5 +14,5 @@ def test_true():
 def test_ddg0():
     resp = requests.get(url_ddg + "/?q=George Washington&format=json")
     rsp_data = resp.json()
-    assert "George Washington" in rsp_data["Header"]
+    assert "George Washington" in rsp_data["RelatedTopics"]
 
