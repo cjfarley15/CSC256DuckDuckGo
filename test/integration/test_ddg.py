260808e20cdce2cfd616de66e85dd2116074c3d3
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 23:09:52 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -1,12 +1,12 @@
 import requests
 
-url_ddg = "https://api.duckduckgo.com"
+url_ddg = "https://api.duckduckgo.com/?q=Presidents+Of+The+United+States&format=json&pretty=1"
 
 
 def test_pres():
-    resp = requests.get(url_ddg + "/?q=Presidents+Of+The+United+States&format=json&pretty=1")
+    resp = requests.get(url_ddg)
     rsp_data = resp.json()
-    assert "Lincoln" in rsp_data ["RelatedTopics"] 
+    assert "Lincoln" in rsp_data  
     
 
 
