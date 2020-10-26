1f7b781fadfd248d55a7901b31e4933a8bbeebce
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 22:14:07 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -14,5 +14,5 @@ def test_true():
 def test_ddg0():
     resp = requests.get(url_ddg + "/?q=DuckDuckGo&format=json")
     rsp_data = resp.json()
-    assert "DuckDuckGo" in rsp_data["RelatedTopics"]
+    assert "DuckDuckGo" in rsp_data["Heading"]
 
