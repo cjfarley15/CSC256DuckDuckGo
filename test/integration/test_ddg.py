ae20f1ba1f9ed93d14901fba7d4c81f10e82373b
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 23:06:48 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -4,7 +4,7 @@ url_ddg = "https://api.duckduckgo.com"
 
 
 def test_pres():
-    resp = requests.get(url_ddg + "/?q=Presidents+Of+The+United+States&format=json&pretty=1&skip")
+    resp = requests.get(url_ddg + "/?q=Presidents+Of+The+United+States&format=json&pretty=1")
     rsp_data = resp.json()
     assert "Lincoln" in rsp_data ["RelatedTopics"] 
     
