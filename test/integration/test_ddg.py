0406efb541fb142b7d3c5ce781b593b73871a32b
cjfarley15 <71407990+cjfarley15@users.noreply.github.com>
2020-10-25 23:04:08 -0400

    Update test_ddg.py

--- test/integration/test_ddg.py
+++ test/integration/test_ddg.py
@@ -6,24 +6,7 @@ url_ddg = "https://api.duckduckgo.com"
 def test_pres():
     resp = requests.get(url_ddg + "/?q=Presidents+Of+The+United+States&format=json&pretty=1&skip")
     rsp_data = resp.json()
-    assert "Washingon" in rsp_data {
-        Abstract: ""
-        AbstractText: ""
-        AbstractSource: ""
-        AbstractURL: ""
-        Image: ""
-        Heading: ""
-        Answer: ""
-        Redirect: ""
-        AnswerType: ""
-        Definition: ""
-        DefinitionSource: ""
-        DefinitionURL: ""
-        RelatedTopics: [ ]
-        Results: [ ]
-        Type: ""
-    }
-        
+    assert "Lincoln" in rsp_data ["RelatedTopics"] 
     
 
 
